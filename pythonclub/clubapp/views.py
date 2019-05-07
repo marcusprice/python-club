99b8c8260ef585291b56bb4be1b7be2268419cc5
Marcus Price <marcusprice88@gmail.com>
2019-05-07 10:22:55 -0700

    Assignment 6 Meeting and Detail

--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -8,3 +8,14 @@ def index(request):
 def getresources(request):
     resource_list=Resource.objects.all()
     return render(request, 'clubapp/resources.html' , {'resource_list' : resource_list})
+
+def getmeetings(request):
+    meetings_list=Meeting.objects.all()
+    return render(request, 'clubapp/meetings.html', {'meetings_list': meetings_list})
+
+def meetingdetails(request, id):
+    meet=get_object_or_404(Meeting, pk=id)
+    context={
+        'meeting' : meet
+    }
+    return render(request, 'clubapp/meetingdetails.html', context=context)
