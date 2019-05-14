2a779076d274e5290417d48a81dde20df45b2f8d
Marcus Price <marcusprice88@gmail.com>
2019-05-14 10:19:44 -0700

    Assignment 7 Tests

--- pythonclub/clubapp/tests.py
+++ pythonclub/clubapp/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your tests here.
+class MeetingTest(TestCase):
+    def test_string(self):
+        type=Meeting(meetingtitle="Functions Meeting")
+        self.assertEqual(str(type), type.meetingtitle)
+
+    def test_table(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
+
+class MeetingMinutesTest(TestCase):
+    def test_string(self):
+        type=MeetingMinutes(minutestext="Lorum Ipsum")
+        self.assertEqual(str(type), type.minutestext)
+
+    def test_table(self):
+        self.assertEqual(str(MeetingMinutes._meta.db_table), 'meetingminutes')
+
+class ResourceTest(TestCase):
+    def test_string(self):
+        type=Resource(resourcename="Laptop")
+        self.assertEqual(str(type), type.resourcename)
+
+    def test_table(self):
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
+
+class EventTest(TestCase):
+    def test_string(self):
+        type=Event(eventtitle="Python and Pizza")
+        self.assertEqual(str(type), type.eventtitle)
+
+    def test_table(self):
+        self.assertEqual(str(Event._meta.db_table), 'event')
